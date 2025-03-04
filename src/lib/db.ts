20a360c678b09241da7b31b9a5cac8fc1db86983
Sulton <sultonalidrus5@gmail.com>
2025-03-04 16:50:46 +0900

    ref : OPTIMIZE prisma

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,7 +1,5 @@
-import { OPTIMIZE_API_KEY } from '$env/static/private';
 import { PrismaClient } from '@prisma/client';
 import { withAccelerate } from '@prisma/extension-accelerate';
-import { withOptimize } from '@prisma/extension-optimize';
 
 const db = new PrismaClient({
 	log: [
@@ -10,12 +8,6 @@ const db = new PrismaClient({
 		{ level: 'warn', emit: 'event' },
 		{ level: 'error', emit: 'event' }
 	]
-})
-	.$extends(
-		withOptimize({
-			apiKey: OPTIMIZE_API_KEY
-		})
-	)
-	.$extends(withAccelerate());
+}).$extends(withAccelerate());
 
 export { db };
